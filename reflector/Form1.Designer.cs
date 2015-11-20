6e47c05a5ce9a868e7dc6281d98ee3a6ba6861cd
JonHolt <jonathanholt92@gmail.com>
2015-11-20 09:25:23 -0700

    it works, needs optimization

---
+++ reflector/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace reflector {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rbRight = new System.Windows.Forms.RadioButton();
+            this.rbLeft = new System.Windows.Forms.RadioButton();
+            this.picImage = new System.Windows.Forms.PictureBox();
+            this.trkSlider = new System.Windows.Forms.TrackBar();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trkSlider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(246, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoScroll = true;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.rbRight);
+            this.panel1.Controls.Add(this.rbLeft);
+            this.panel1.Controls.Add(this.picImage);
+            this.panel1.Controls.Add(this.trkSlider);
+            this.panel1.Location = new System.Drawing.Point(0, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(246, 310);
+            this.panel1.TabIndex = 1;
+            // 
+            // rbRight
+            // 
+            this.rbRight.AutoSize = true;
+            this.rbRight.Location = new System.Drawing.Point(162, 3);
+            this.rbRight.Name = "rbRight";
+            this.rbRight.Size = new System.Drawing.Size(74, 17);
+            this.rbRight.TabIndex = 3;
+            this.rbRight.Text = "Right Side";
+            this.rbRight.UseVisualStyleBackColor = true;
+            this.rbRight.CheckedChanged += new System.EventHandler(this.rbRight_CheckedChanged);
+            // 
+            // rbLeft
+            // 
+            this.rbLeft.AutoSize = true;
+            this.rbLeft.Checked = true;
+            this.rbLeft.Location = new System.Drawing.Point(3, 3);
+            this.rbLeft.Name = "rbLeft";
+            this.rbLeft.Size = new System.Drawing.Size(67, 17);
+            this.rbLeft.TabIndex = 2;
+            this.rbLeft.TabStop = true;
+            this.rbLeft.Text = "Left Side";
+            this.rbLeft.UseVisualStyleBackColor = true;
+            this.rbLeft.CheckedChanged += new System.EventHandler(this.rbLeft_CheckedChanged);
+            // 
+            // picImage
+            // 
+            this.picImage.Location = new System.Drawing.Point(3, 83);
+            this.picImage.Name = "picImage";
+            this.picImage.Size = new System.Drawing.Size(235, 219);
+            this.picImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picImage.TabIndex = 1;
+            this.picImage.TabStop = false;
+            // 
+            // trkSlider
+            // 
+            this.trkSlider.Location = new System.Drawing.Point(2, 32);
+            this.trkSlider.Name = "trkSlider";
+            this.trkSlider.Size = new System.Drawing.Size(234, 45);
+            this.trkSlider.TabIndex = 0;
+            this.trkSlider.Scroll += new System.EventHandler(this.trkSlider_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(246, 337);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Reflectotron";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trkSlider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox picImage;
+        private System.Windows.Forms.TrackBar trkSlider;
+        private System.Windows.Forms.RadioButton rbRight;
+        private System.Windows.Forms.RadioButton rbLeft;
+    }
+}
+
---
+++ reflector/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace reflector {
+    public partial class Form1 : Form {
+        Bitmap image, reflected;
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e) {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Jpeg (.jpg)|*.jpg";
+            dialog.ShowDialog();
+            image = (Bitmap)Image.FromFile(dialog.FileName);
+
+            trkSlider.SetRange(0, image.Width);
+            trkSlider.Value = image.Width / 2;
+
+            if (rbLeft.Checked)
+                reflectLeft();
+            else
+                reflectRight();
+            picImage.Image = reflected;
+            trkSlider.Width = image.Width;
+        }
+
+        private void reflectLeft() {
+            int mirror = trkSlider.Value;
+            reflected = new Bitmap(mirror * 2, image.Height);
+            for(int i = 0; i < reflected.Height; ++i) {
+                int j = mirror - 1 , k = mirror;
+                while(j >=0 && k < reflected.Width) {
+                    reflected.SetPixel(j, i, image.GetPixel(j, i));
+                    reflected.SetPixel(k, i, image.GetPixel(j, i));
+                    --j;
+                    ++k;
+                }
+            }
+        }
+
+        private void reflectRight() {
+            int mirror = trkSlider.Value;
+            reflected = new Bitmap(mirror * 2, image.Height);
+            for (int i = 0; i < reflected.Height; ++i) {
+                int j = mirror - 1, k = mirror;
+                while (j >= 0 && k < reflected.Width && k < image.Width) {
+                    reflected.SetPixel(j, i, image.GetPixel(k, i));
+                    reflected.SetPixel(k, i, image.GetPixel(k, i));
+                    --j;
+                    ++k;
+                }
+            }
+        }
+
+        private void trkSlider_Scroll(object sender, EventArgs e) {
+            if (rbLeft.Checked)
+                reflectLeft();
+            else
+                reflectRight();
+            picImage.Image = reflected;
+        }
+
+        private void rbLeft_CheckedChanged(object sender, EventArgs e) {
+            if (rbLeft.Checked)
+                reflectLeft();
+            else
+                reflectRight();
+            picImage.Image = reflected;
+        }
+
+        private void rbRight_CheckedChanged(object sender, EventArgs e) {
+            if (rbLeft.Checked)
+                reflectLeft();
+            else
+                reflectRight();
+            picImage.Image = reflected;
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e) {
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "Jpeg (.jpg)|*.jpg";
+            save.ShowDialog();
+
+            reflected.Save(save.FileName);
+        }
+    }
+}
